68708a1020df22b2d9304806ca1354739b59ce46
Szilvia Lőrincz <lorszil@gmail.com>
2017-08-01 09:31:17 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+    $host        = "host = 127.0.0.1";
+    $port        = "port = 5432";
+    $dbname      = "dbname = greenroom";
+    $credentials = "user = postgres password=12345";
+
+    $db = pg_connect( "$host $port $dbname $credentials"  );
+    if(!$db) {
+        echo "Error : Unable to open database\n";
+    } else {
+        echo "Opened database successfully\n";
+    }
+
+    // Performing SQL query
+    $query = 'SELECT * FROM TRACTOR';
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+    // Printing results in HTML
+    echo "<table>\n";
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        echo "\t<tr>\n";
+        foreach ($line as $col_value) {
+            echo "\t\t<td>$col_value</td>\n";
+        }
+        echo "\t</tr>\n";
+    }
+    echo "</table>\n";
+
+    // Free resultset
+    pg_free_result($result);
+
+
+    pg_close($db);
